9e49c3ed5f49446ecfc2030d971f44705cbc13ba
AlexandrV <borisov.alexandr.7@gmail.com>
2018-01-17 13:12:11 +0500

    Fix

--- src/gui/pages_maped/mission/KM_GUIMapEdMissionAlliances.pas
+++ src/gui/pages_maped/mission/KM_GUIMapEdMissionAlliances.pas
@@ -15,8 +15,8 @@ type
     procedure Mission_AlliancesChange(Sender: TObject);
     procedure Mission_AlliancesUpdate;
   protected
-    Panel_Alliances: TKMPanel;
     PopUp_Alliances: TKMPopUpMenu;
+    Panel_Alliances: TKMPanel;
       Bevel_Alliances: TKMBevel;
       Image_Alliances: TKMImage;
       CheckBox_Alliances: array [0..MAX_HANDS-1, 0..MAX_HANDS-1] of TKMCheckBox;
@@ -37,6 +37,15 @@ implementation
 uses
   KM_HandsCollection, KM_ResTexts, KM_RenderUI, KM_ResFonts, KM_Hand, KM_ResKeys;
 
+const
+  TB_CHB_A_L = 32; //Left CheckBox_Alliances
+  TB_CHB_A_T = 40; //Top CheckBox_Alliances
+  TB_CHB_A_W = 20; // Spacing between CheckBox_Alliances by width
+  TB_CHB_A_H = 20; // Distance between CheckBox_Alliances by height
+  TB_PANEL_A_L = 30; //Panel_Alliances.left
+  TB_PANEL_A_T = 30; // Panel_Alliances.top
+  TB_BUTTON_W = 200; // Button_CloseAlliances.Width
+  TB_BUTTON_H = 30; // Button_CloseAlliances.Height
 
 { TKMMapEdMissionAlliances }
 constructor TKMMapEdMissionAlliances.Create(aParent: TKMPanel);
@@ -45,8 +54,9 @@ var
 begin
   inherited Create;
 
-  PopUp_Alliances := TKMPopUpMenu.Create(aParent.MasterParent, 330);
-  PopUp_Alliances.Height := 420;
+  PopUp_Alliances := TKMPopUpMenu.Create(aParent.MasterParent, 0);
+  PopUp_Alliances.Width := (TB_PANEL_A_L * 2) + (TB_CHB_A_L * 2) + (TB_CHB_A_W * MAX_HANDS);
+  PopUp_Alliances.Height := (TB_PANEL_A_T * 2) + (TB_CHB_A_T * 2) + (TB_CHB_A_H * MAX_HANDS) + TB_CHB_A_H + 10;
   PopUp_Alliances.AnchorsCenter;
   PopUp_Alliances.Left := (aParent.MasterParent.Width div 2) - (PopUp_Alliances.Width div 2);
   PopUp_Alliances.Top := (aParent.MasterParent.Height div 2) - (PopUp_Alliances.Height div 2);
@@ -58,16 +68,16 @@ begin
     Image_Alliances := TKMImage.Create(PopUp_Alliances, 0, 0, PopUp_Alliances.Width, PopUp_Alliances.Height, 3, rxGuiMain);
     Image_Alliances.ImageStretch;
 
-  Panel_Alliances := TKMPanel.Create(PopUp_Alliances, 30, 30, PopUp_Alliances.Width - 60, PopUp_Alliances.Height - 60);
+  Panel_Alliances := TKMPanel.Create(PopUp_Alliances, TB_PANEL_A_L, TB_PANEL_A_T, PopUp_Alliances.Width - (TB_PANEL_A_L * 2), PopUp_Alliances.Height - (TB_PANEL_A_T * 2));
 
-  TKMLabel.Create(Panel_Alliances, 0, PAGE_TITLE_Y, Panel_Alliances.Width, 0, gResTexts[TX_MAPED_ALLIANCE], fnt_Outline, taCenter);
+  TKMLabel.Create(Panel_Alliances, 0, 0, Panel_Alliances.Width, 0, gResTexts[TX_MAPED_ALLIANCE], fnt_Outline, taCenter);
   for I := 0 to MAX_HANDS - 1 do
   begin
-    TKMLabel.Create(Panel_Alliances, 40 + I * 20, 30, IntToStr(I + 1), fnt_Grey, taCenter);
-    TKMLabel.Create(Panel_Alliances, 10, 50 + I * 20, IntToStr(I + 1), fnt_Grey, taCenter);
+    TKMLabel.Create(Panel_Alliances, 40 + I * TB_CHB_A_W, 20, IntToStr(I + 1), fnt_Grey, taCenter);
+    TKMLabel.Create(Panel_Alliances, 10, 40 + I * TB_CHB_A_H, IntToStr(I + 1), fnt_Grey, taCenter);
     for K := 0 to MAX_HANDS - 1 do
     begin
-      CheckBox_Alliances[I,K] := TKMCheckBox.Create(Panel_Alliances, 32 + K * 20, 50 + I * 20, 30, 30, '', fnt_Metal);
+      CheckBox_Alliances[I,K] := TKMCheckBox.Create(Panel_Alliances, TB_CHB_A_L + K * TB_CHB_A_W, TB_CHB_A_T + I * TB_CHB_A_H, 30, 30, '', fnt_Metal);
       CheckBox_Alliances[I,K].Tag       := I * MAX_HANDS + K;
       CheckBox_Alliances[I,K].OnClick   := Mission_AlliancesChange;
     end;
@@ -75,12 +85,12 @@ begin
 
   //It does not have OnClick event for a reason:
   // - we don't have a rule to make alliances symmetrical yet
-  CheckBox_AlliancesSym := TKMCheckBox.Create(Panel_Alliances, Panel_Alliances.Center.X - 105, Panel_Alliances.Height - 60, 150, 20, gResTexts[TX_MAPED_ALLIANCE_SYMMETRIC], fnt_Metal);
+  CheckBox_AlliancesSym := TKMCheckBox.Create(Panel_Alliances, (Panel_Alliances.Width div 2) - 75, Panel_Alliances.Height - 60, 150, 20, gResTexts[TX_MAPED_ALLIANCE_SYMMETRIC], fnt_Metal);
   CheckBox_AlliancesSym.Checked := True;
   CheckBox_AlliancesSym.Disable;
 
-  Button_CloseAlliances := TKMButton.Create(Panel_Alliances, Panel_Alliances.Center.X - 130, Panel_Alliances.Height - 30,
-                                            200, 30, gResTexts[TX_MAPED_TERRAIN_CLOSE_PALETTE], bsGame);
+  Button_CloseAlliances := TKMButton.Create(Panel_Alliances, (Panel_Alliances.Width div 2) - (TB_BUTTON_W div 2), Panel_Alliances.Height - 30,
+                                            TB_BUTTON_W, TB_BUTTON_H, gResTexts[TX_MAPED_TERRAIN_CLOSE_PALETTE], bsGame);
   Button_CloseAlliances.OnClick := CloseAlliances_Click;
 end;
 
