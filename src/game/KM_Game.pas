reyandme/kam_remake

commit a6559b48c419b71168c0300ac5e769d6eae15bd2

Fix for eyedropper tool was not working on the newly created map

src/game/KM_Game.pas
@@ -1270,6 +1270,7 @@ begin
   fSaveFile := '';
 
   fMapEditor := TKMMapEditor.Create(True, fTerrainPainter, fMapEditorInterface.HistoryUndoRedo, fMapEditorInterface.HistoryAddCheckpoint);
+  fMapEditor.OnEyedropper := fMapEditorInterface.GuiTerrain.GuiTiles.TilesTableSetTileTexId;
   fMapEditor.MissionDefSavePath := fParams.Name + '.dat';
   gTerrain.MakeNewMap(aSizeX, aSizeY, True);
   fTerrainPainter.InitEmpty;

commit 185b5cc5da83712058e135e4445d9a3c0e8a36cb

fix after commit a6559b48c419b71168c0300ac5e769d6eae15bd2 broke RMG in game lobby

src/game/KM_Game.pas
@@ -147,7 +147,7 @@ type
                     aAIType: TKMAIType = aitNone; aAutoselectHumanLoc: Boolean = False);
 
     procedure AfterStart;
-    procedure MapEdStartEmptyMap(aSizeX, aSizeY: Integer);
+    procedure MapEdStartEmptyMap(aSizeX, aSizeY: Integer; aIsRMG: Boolean);
     procedure LoadFromStream(var LoadStream: TKMemoryStream);
     procedure LoadFromFile(const aPathName: UnicodeString; const aCustomReplayFile: UnicodeString = '');
     procedure LoadSavePoint(aTick: Cardinal; const aSaveFile: UnicodeString);
@@ -1260,7 +1260,7 @@ end;
 
 
 //Start MapEditor (empty map)
-procedure TKMGame.MapEdStartEmptyMap(aSizeX, aSizeY: Integer);
+procedure TKMGame.MapEdStartEmptyMap(aSizeX, aSizeY: Integer; aIsRMG: Boolean);
 var
   I: Integer;
 begin
@@ -1270,7 +1270,8 @@ begin
   fSaveFile := '';
 
   fMapEditor := TKMMapEditor.Create(True, fTerrainPainter, fMapEditorInterface.HistoryUndoRedo, fMapEditorInterface.HistoryAddCheckpoint);
-  fMapEditor.OnEyedropper := fMapEditorInterface.GuiTerrain.GuiTiles.TilesTableSetTileTexId;
+  if not aIsRMG then
+    fMapEditor.OnEyedropper := fMapEditorInterface.GuiTerrain.GuiTiles.TilesTableSetTileTexId;
   fMapEditor.MissionDefSavePath := fParams.Name + '.dat';
   gTerrain.MakeNewMap(aSizeX, aSizeY, True);
   fTerrainPainter.InitEmpty;

src/game/KM_GameApp.pas
@@ -861,7 +861,7 @@ begin
   CreateGame(aGameMode);
   gGame.SetSeed(4); //Every time the game will be the same as previous. Good for debug.
   try
-    gGame.MapEdStartEmptyMap(aSizeX, aSizeY);
+    gGame.MapEdStartEmptyMap(aSizeX, aSizeY, False);
   except
     on E : Exception do
     begin

src/gui/pages_maped/terrain/KM_GUIMapEdRMG.pas
@@ -692,7 +692,7 @@ begin
           4: begin MapX := 256; MapY := 256; end;
         end;
       // Create empty map in background
-      gGame.MapEdStartEmptyMap(MapX, MapY);
+      gGame.MapEdStartEmptyMap(MapX, MapY, True);
       // Get RMG config
       GetSettingsFromGUI();
       // Call RMG