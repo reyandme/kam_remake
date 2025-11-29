c9768fcef083222f7bc4919cbbe3e491e9524cbb
kromster80 <kromster80@gmail.com>
2025-11-29 16:07:17 +0100

    Cleanup

--- src/ext/KromIOUtils.pas
+++ src/ext/KromIOUtils.pas
@@ -1,47 +1,54 @@
 unit KromIOUtils;
-{$IFDEF VER140} {$DEFINE WDC} {$ENDIF}  // Delphi 6
-{$IFDEF VER150} {$DEFINE WDC} {$ENDIF}  // Delphi 7
 interface
-uses ShellAPI, SysUtils;
+uses
+  ShellAPI, SysUtils;
 
-function CopyDir(const fromDir, toDir: string): Boolean;
-function DelDir(dir: string): Boolean;
-
-function ReturnSize(i: int64): string;
+function CopyDir(const aFromDir, aToDir: string): Boolean;
+function DelDir(aDir: string): Boolean;
+function ReturnSize(aSize: Int64): string;
 
 implementation
 
-function CopyDir(const fromDir, toDir: string): Boolean;
-var fos: TSHFileOpStruct;
+
+function CopyDir(const aFromDir, aToDir: string): Boolean;
+var
+  fos: TSHFileOpStruct;
 begin
-  FillChar(fos, SizeOf(fos), #0);
+  fos := default(TSHFileOpStruct);
   fos.wFunc  := FO_COPY;
   fos.fFlags := FOF_FILESONLY;
-  fos.pFrom  := PChar(fromDir + #0);
-  fos.pTo    := PChar(toDir);
+  fos.pFrom  := PChar(aFromDir + #0);
+  fos.pTo    := PChar(aToDir);
   Result := ShFileOperation(fos) = 0;
 end;
 
-function DelDir(dir: string): Boolean;
-var fos: TSHFileOpStruct;
+
+function DelDir(aDir: string): Boolean;
+var
+  fos: TSHFileOpStruct;
 begin
-  FillChar(fos, SizeOf(fos), #0);
+  fos := default(TSHFileOpStruct);
   fos.wFunc  := FO_DELETE;
   fos.fFlags := FOF_SILENT or FOF_NOCONFIRMATION;
-  fos.pFrom  := PChar(dir + #0);
+  fos.pFrom  := PChar(aDir + #0);
   Result := ShFileOperation(fos) = 0;
 end;
 
-function ReturnSize(i: int64): string;
+
+function ReturnSize(aSize: Int64): string;
 begin
-  if i<2048 then Result:=inttostr(i)+'b'
+  if aSize < 2048 then
+    Result := IntToStr(aSize) + 'b'
   else
-  if i<2048*1000 then Result:=inttostr(i shr 10)+'Kb'
+  if aSize < 2048*1000 then
+    Result := IntToStr(aSize shr 10) + 'Kb'
   else
-  if i<2048*1000000 then Result:=inttostr(i shr 20)+'Mb'
+  if aSize < 2048*1000000 then
+    Result := IntToStr(aSize shr 20) + 'Mb'
   else
-  Result:=inttostr(i shr 30)+'Gb';
+    Result := IntToStr(aSize shr 30) + 'Gb';
 end;
+
 
 end.
  
