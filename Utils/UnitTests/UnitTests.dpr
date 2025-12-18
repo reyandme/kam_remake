222e9d2c39d47900c23949c615a2dd8896bea434
kromster80 <kromster80@gmail.com>
2025-12-18 15:20:05 +0100

    "Removed" tests that are all too old and are not pure unit-tests

--- Utils/UnitTests/UnitTests.dpr
+++ Utils/UnitTests/UnitTests.dpr
@@ -11,14 +11,10 @@ uses
   TestFramework,
   GUITestRunner,
   TextTestRunner,
-  TestKM_Campaigns in 'TestKM_Campaigns.pas',
   TestKM_CommonClasses in 'TestKM_CommonClasses.pas',
   TestKM_CommonUtils in 'TestKM_CommonUtils.pas',
-  TestKM_FogOfWar in 'TestKM_FogOfWar.pas',
   TestKM_Points in 'TestKM_Points.pas',
-  TestKM_Utils in 'TestKM_Utils.pas',
-  TestKM_UnitActionGoInOut in 'TestKM_UnitActionGoInOut.pas'{,
-  TestKM_MessageStack in 'TestKM_MessageStack.pas'};
+  TestKM_Utils in 'TestKM_Utils.pas';
 
 {$R *.RES}
 
