0ae1e2bb7f1c2090708b1b6bf7312b01ab355573
kromster80 <kromster80@gmail.com>
2026-01-18 16:54:12 +0100

    Cleanups

--- src/units/actions/KM_UnitActionStay.pas
+++ src/units/actions/KM_UnitActionStay.pas
@@ -2,19 +2,18 @@ unit KM_UnitActionStay;
 {$I KaM_Remake.inc}
 interface
 uses
-  Classes, KM_Defaults, KromUtils, KM_CommonClasses, KM_Units, SysUtils, Math, KM_Points;
+  KM_Defaults, KM_CommonClasses, KM_Units;
 
 type
-  {Stay in place for set time}
+  // Stay in place for set time
   TKMUnitActionStay = class(TKMUnitAction)
   private
-    StayStill:boolean;
-    TimeToStay:integer;
-    StillFrame:byte;
+    fStayStill: Boolean;
+    fTimeToStay: Integer;
+    fStillFrame: Byte;
     procedure MakeSound(Cycle, Step: Byte);
   public
-    constructor Create(aUnit: TKMUnit; aTimeToStay: Integer; aActionType: TKMUnitActionType; aStayStill: Boolean;
-                       aStillFrame: Byte; aLocked: Boolean);
+    constructor Create(aUnit: TKMUnit; aTimeToStay: Integer; aActionType: TKMUnitActionType; aStayStill: Boolean; aStillFrame: Byte; aLocked: Boolean);
     constructor Load(LoadStream: TKMemoryStream); override;
     function ActName: TKMUnitActionName; override;
     function CanBeInterrupted(aForced: Boolean = True): Boolean; override;
@@ -22,24 +21,24 @@ type
     function Execute: TKMActionResult; override;
     procedure Save(SaveStream: TKMemoryStream); override;
 
-    function ObjToStringShort(const aSeparator: String = ' '): String; override;
+    function ObjToStringShort(const aSeparator: string = ' '): string; override;
   end;
 
 
 implementation
 uses
-  KM_HandsCollection, KM_Sound, KM_ResSound, KM_Resource, KM_ResUnits;
+  Math, SysUtils,
+  KM_Points, KM_HandsCollection, KM_Sound, KM_Resource, KM_ResSound;
 
 
-{ TUnitActionStay }
-constructor TKMUnitActionStay.Create(aUnit: TKMUnit; aTimeToStay: Integer; aActionType: TKMUnitActionType; aStayStill: Boolean;
-                                     aStillFrame: Byte; aLocked: Boolean);
+{ TKMUnitActionStay }
+constructor TKMUnitActionStay.Create(aUnit: TKMUnit; aTimeToStay: Integer; aActionType: TKMUnitActionType; aStayStill: Boolean; aStillFrame: Byte; aLocked: Boolean);
 begin
   inherited Create(aUnit, aActionType, aLocked);
 
-  StayStill   := aStayStill;
-  TimeToStay  := aTimeToStay;
-  StillFrame  := aStillFrame;
+  fStayStill   := aStayStill;
+  fTimeToStay  := aTimeToStay;
+  fStillFrame  := aStillFrame;
 end;
 
 
@@ -48,9 +47,9 @@ begin
   inherited;
 
   LoadStream.CheckMarker('UnitActionStay');
-  LoadStream.Read(StayStill);
-  LoadStream.Read(TimeToStay);
-  LoadStream.Read(StillFrame);
+  LoadStream.Read(fStayStill);
+  LoadStream.Read(fTimeToStay);
+  LoadStream.Read(fStillFrame);
 end;
 
 
@@ -71,25 +70,26 @@ begin
   if SKIP_SOUND then Exit;
 
   //Do not play sounds if unit is invisible to gMySpectator
-  if gMySpectator.FogOfWar.CheckTileRevelation(fUnit.Position.X, fUnit.Position.Y) < 255 then exit;
+  if gMySpectator.FogOfWar.CheckTileRevelation(fUnit.Position.X, fUnit.Position.Y) < 255 then Exit;
 
   //Various UnitTypes and ActionTypes produce all the sounds
   case fUnit.UnitType of
-    utBuilder:      case ActionType of
-                      uaWork:  if Step = 3 then gSoundPlayer.Play(sfxHouseBuild, fUnit.PositionF);
-                      uaWork1: if Step = 0 then gSoundPlayer.Play(sfxDig, fUnit.PositionF);
-                      uaWork2: if Step = 8 then gSoundPlayer.Play(sfxPave, fUnit.PositionF);
-                    end;
-    utFarmer:      case ActionType of
-                      uaWork:  if Step = 8 then gSoundPlayer.Play(sfxCornCut, fUnit.PositionF);
-                      uaWork1: if Step = 0 then gSoundPlayer.Play(sfxCornSow, fUnit.PositionF,True,0.6);
-                    end;
+    utBuilder:    case ActionType of
+                    uaWork:  if Step = 3 then gSoundPlayer.Play(sfxHouseBuild, fUnit.PositionF);
+                    uaWork1: if Step = 0 then gSoundPlayer.Play(sfxDig, fUnit.PositionF);
+                    uaWork2: if Step = 8 then gSoundPlayer.Play(sfxPave, fUnit.PositionF);
+                  end;
+    utFarmer:     case ActionType of
+                    uaWork:  if Step = 8 then gSoundPlayer.Play(sfxCornCut, fUnit.PositionF);
+                    uaWork1: if Step = 0 then gSoundPlayer.Play(sfxCornSow, fUnit.PositionF, True, 0.6);
+                  end;
     utStonemason: if ActionType = uaWork then
-                      if Step = 3 then gSoundPlayer.Play(sfxMinestone, fUnit.PositionF,True,1.4);
-    utWoodCutter:  case ActionType of
-                      uaWork: if (fUnit.AnimStep mod Cycle = 3) and (fUnit.Direction <> dirN) then gSoundPlayer.Play(sfxChopTree, fUnit.PositionF,True)
-                      else     if (fUnit.AnimStep mod Cycle = 0) and (fUnit.Direction =  dirN) then gSoundPlayer.Play(sfxWoodcutterDig, fUnit.PositionF,True);
-                    end;
+                    if Step = 3 then gSoundPlayer.Play(sfxMinestone, fUnit.PositionF, True, 1.4);
+    utWoodCutter: case ActionType of
+                    uaWork: if (fUnit.AnimStep mod Cycle = 3) and (fUnit.Direction <> dirN) then gSoundPlayer.Play(sfxChopTree, fUnit.PositionF, True)
+                  else
+                    if (fUnit.AnimStep mod Cycle = 0) and (fUnit.Direction = dirN) then gSoundPlayer.Play(sfxWoodcutterDig, fUnit.PositionF, True);
+                  end;
   end;
 end;
 
@@ -98,25 +98,24 @@ function TKMUnitActionStay.Execute: TKMActionResult;
 var
   cycle, step: Byte;
 begin
-  if not StayStill then
+  if not fStayStill then
   begin
     cycle := Max(gRes.Units[fUnit.UnitType].UnitAnim[ActionType, fUnit.Direction].Count, 1);
     step  := fUnit.AnimStep mod cycle;
 
     StepDone := fUnit.AnimStep mod cycle = 0;
 
-    if TimeToStay >= 1 then MakeSound(cycle, step);
+    if fTimeToStay >= 1 then MakeSound(cycle, step);
 
     Inc(fUnit.AnimStep);
-  end
-  else
+  end else
   begin
-    fUnit.AnimStep := StillFrame;
+    fUnit.AnimStep := fStillFrame;
     StepDone := True;
   end;
 
-  Dec(TimeToStay);
-  if TimeToStay <= 0 then
+  Dec(fTimeToStay);
+  if fTimeToStay <= 0 then
     Result := arActDone
   else
     Result := arActContinues;
@@ -128,9 +127,9 @@ begin
   inherited;
 
   SaveStream.PlaceMarker('UnitActionStay');
-  SaveStream.Write(StayStill);
-  SaveStream.Write(TimeToStay);
-  SaveStream.Write(StillFrame);
+  SaveStream.Write(fStayStill);
+  SaveStream.Write(fTimeToStay);
+  SaveStream.Write(fStillFrame);
 end;
 
 
@@ -140,13 +139,13 @@ begin
 end;
 
 
-function TKMUnitActionStay.ObjToStringShort(const aSeparator: String): String;
+function TKMUnitActionStay.ObjToStringShort(const aSeparator: string): string;
 begin
   Result := inherited + Format('%s[StayStill = %s%sTimeToStay = %d%sStillFrame = %d]', [
                                aSeparator,
-                               BoolToStr(StayStill, True), aSeparator,
-                               TimeToStay, aSeparator,
-                               StillFrame]);
+                               BoolToStr(fStayStill, True), aSeparator,
+                               fTimeToStay, aSeparator,
+                               fStillFrame]);
 end;
 
 
