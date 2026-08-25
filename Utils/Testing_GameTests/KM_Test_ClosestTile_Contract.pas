1be6875d46cf98fd46962578da18c432ba553245
kromster80 <kromster80@gmail.com>
2026-08-25 19:25:12 +0200

    Test cleaned up

--- Utils/Testing_GameTests/KM_Test_ClosestTile_Contract.pas
+++ Utils/Testing_GameTests/KM_Test_ClosestTile_Contract.pas
@@ -2,21 +2,18 @@ unit KM_Test_ClosestTile_Contract;
 {$I KaM_Remake.inc}
 interface
 uses
-  KM_Test;
+  KM_Test,
+  KM_Points;
 
 
 type
-  // Pins down what GetClosestTile promises its callers, on plain grass where every tile is walk
-  // connected: a free reachable target comes back as is, and a target somebody else stands on
-  // gets traded for a free tile next to it.
+  // Check GetClosestTile
   TKMTest_ClosestTileContract = class(TKMTest)
-  private const
-    ORIGIN_X = 20;  ORIGIN_Y = 20;  // Where our own unit stands
-    FREE_X   = 40;  FREE_Y   = 40;  // Empty tile with nobody around it
-    BUSY_X   = 40;  BUSY_Y   = 20;  // Tile with another unit standing on it
   protected
+    fOrigin: TKMPoint;
+    fFreeLoc: TKMPoint;
+    fBusyLoc: TKMPoint;
     procedure SetUp; override;
-    procedure TearDown; override;
     procedure DoTick(aTick: Cardinal; var aKeepGoing: Boolean); override;
     procedure CheckResult; override;
   public
@@ -28,9 +25,8 @@ type
 implementation
 uses
   SysUtils,
-  KM_Defaults, KM_Points,
-  KM_GameApp, KM_HandsCollection, KM_HandTypes, KM_Terrain,
-  KM_UnitWarrior;
+  KM_Defaults,
+  KM_GameApp, KM_HandsCollection, KM_HandTypes, KM_Terrain;
 
 
 { TKMTest_ClosestTileContract }
@@ -38,30 +34,14 @@ procedure TKMTest_ClosestTileContract.SetUp;
 begin
   inherited;
 
-  DYNAMIC_TERRAIN := False;
+  gGameApp.NewGameEmptyMap(32, 32);
 
-  // Terrain is fully prepared here, one tick is enough
-  fDuration := 5;
+  fOrigin := TKMPoint.New(12, 12);
+  fFreeLoc := TKMPoint.New(16, 16);
+  fBusyLoc := TKMPoint.New(20, 20);
 
-  gGameApp.NewGameEmptyMap(64, 64);
-
-  // Player controlled, so that no AI general orders the squads about
-  gHands[0].HandType := hndHuman;
-
-  // Warriors need a group, CheckForEnemy calls OnPickedFight without an Assigned check
-  var origin := TKMUnitWarrior(gHands[0].AddUnit(utAxeFighter, KMPoint(ORIGIN_X, ORIGIN_Y), False));
-  gHands[0].UnitGroups.AddGroup(origin);
-
-  // Stands right on top of the busy target tile
-  var blocker := TKMUnitWarrior(gHands[0].AddUnit(utAxeFighter, KMPoint(BUSY_X, BUSY_Y), False));
-  gHands[0].UnitGroups.AddGroup(blocker);
-end;
-
-
-procedure TKMTest_ClosestTileContract.TearDown;
-begin
-  inherited;
-  DYNAMIC_TERRAIN := True;
+  gHands[0].AddUnitGroup(utAxeFighter, fOrigin, dirN, 1, 1);
+  gHands[0].AddUnitGroup(utAxeFighter, fBusyLoc, dirN, 1, 1);
 end;
 
 
@@ -69,35 +49,18 @@ procedure TKMTest_ClosestTileContract.DoTick(aTick: Cardinal; var aKeepGoing: Bo
 begin
   aKeepGoing := False; // Nothing to simulate
 
-  var origin := KMPoint(ORIGIN_X, ORIGIN_Y);
-  var freeLoc := KMPoint(FREE_X, FREE_Y);
-  var busyLoc := KMPoint(BUSY_X, BUSY_Y);
+  AssertTrue(gTerrain.RouteCanBeMade(fOrigin, fFreeLoc, tpWalk), 'Setup: the free target should be reachable');
 
-  // Check the setup itself, so a misplaced unit does not make the test pass silently
-  AssertTrue(gTerrain.HasUnit(origin), 'Setup: our own unit should stand on the origin tile');
-  AssertTrue(not gTerrain.HasUnit(freeLoc), 'Setup: the free target tile should be empty');
-  AssertTrue(gTerrain.HasUnit(busyLoc), 'Setup: the busy target tile should be taken by another unit');
-  AssertTrue(gTerrain.RouteCanBeMade(origin, freeLoc, tpWalk), 'Setup: the free target should be reachable');
+  var closestTile: TKMPoint;
+  closestTile := gTerrain.GetClosestTile(fFreeLoc, fOrigin, tpWalk, False);
+  AssertTrue(KMSamePoint(closestTile, fFreeLoc), Format('GetClosestTile(target %s, origin %s) returned %s instead of the free target itself',
+    [fFreeLoc.ToString, fOrigin.ToString, closestTile.ToString]));
 
-  // Free, walkable, walk connected target - there is nothing better to offer
-  var res := gTerrain.GetClosestTile(freeLoc, origin, tpWalk, False);
-  AssertTrue(KMSamePoint(res, freeLoc),
-    Format('GetClosestTile(target %s, origin %s) returned %s instead of the free target itself',
-           [freeLoc.ToString, origin.ToString, res.ToString]));
-
-  // Target taken by another unit - we are low priority, so we do not bump important units
-  res := gTerrain.GetClosestTile(busyLoc, origin, tpWalk, False);
-  AssertTrue(not KMSamePoint(res, busyLoc),
-    Format('GetClosestTile(target %s) returned the very tile another unit stands on', [busyLoc.ToString]));
-  AssertTrue(not gTerrain.HasUnit(res),
-    Format('GetClosestTile(target %s) returned %s, which is taken by another unit', [busyLoc.ToString, res.ToString]));
-  AssertTrue(res.GetLengthDiag(busyLoc) <= 2,
-    Format('GetClosestTile(target %s) returned %s, too far away to be the closest free tile', [busyLoc.ToString, res.ToString]));
-
-  // The aAcceptTargetLoc shortcut above the loop
-  res := gTerrain.GetClosestTile(freeLoc, origin, tpWalk, True);
-  AssertTrue(KMSamePoint(res, freeLoc),
-    Format('GetClosestTile(target %s, aAcceptTargetLoc = True) returned %s', [freeLoc.ToString, res.ToString]));
+  closestTile := gTerrain.GetClosestTile(fBusyLoc, fOrigin, tpWalk, False);
+  AssertTrue(not KMSamePoint(closestTile, fBusyLoc),
+    Format('GetClosestTile(target %s) returned the very tile another unit stands on', [fBusyLoc.ToString]));
+  AssertTrue(closestTile.GetLengthDiag(fBusyLoc) <= 2,
+    Format('GetClosestTile(target %s) returned %s, too far away to be the closest free tile', [fBusyLoc.ToString, closestTile.ToString]));
 end;
 
 
