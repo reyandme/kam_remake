f5b204122f60ec748f620f1134a93a4064ff5a25
kromster80 <kromster80@gmail.com>
2026-09-18 15:43:13 +0200

    Code and comments formatting

--- src/units/KM_Units.pas
+++ src/units/KM_Units.pas
@@ -186,8 +186,7 @@ type
 
     procedure SetActionWalk(const aLocB: TKMPoint; aActionType: TKMUnitActionType; aDistance: Single; aTargetUnit: TKMUnit;
                             aTargetHouse: TKMHouse; aAvoidLockedByMovementCost: Boolean = True);
-    function TrySetActionWalk(const aLocB: TKMPoint; aActionType: TKMUnitActionType; aDistance: Single; aTargetUnit: TKMUnit;
-                              aTargetHouse: TKMHouse): Boolean;
+    function TrySetActionWalk(const aLocB: TKMPoint; aActionType: TKMUnitActionType; aDistance: Single; aTargetUnit: TKMUnit; aTargetHouse: TKMHouse): Boolean;
     procedure SetActionWalkToHouse(aHouse: TKMHouse; aDistance: Single; aActionType: TKMUnitActionType = uaWalk);
     procedure SetActionWalkFromHouse(aHouse: TKMHouse; aDistance: Single; aActionType: TKMUnitActionType = uaWalk);
     procedure SetActionWalkToUnit(aUnit: TKMUnit; aDistance:single; aActionType: TKMUnitActionType = uaWalk);
@@ -1848,35 +1847,29 @@ begin
 end;
 
 
-//Try to set action walk for unit
-//Used for warriors when they try to update route when attacking house and their destination loc is locked, so they try to avoid it
-function TKMUnit.TrySetActionWalk(const aLocB: TKMPoint; aActionType: TKMUnitActionType; aDistance: Single;
-                                   aTargetUnit: TKMUnit; aTargetHouse: TKMHouse): Boolean; //Silent by default, as we consider fail could happen
-var
-  newAction: TKMUnitActionWalkTo;
+// Try to set the walk action for the unit
+// Used for warriors when they try to update route when attacking a house and their destination Loc is locked, so they try to avoid it
+function TKMUnit.TrySetActionWalk(const aLocB: TKMPoint; aActionType: TKMUnitActionType; aDistance: Single; aTargetUnit: TKMUnit; aTargetHouse: TKMHouse): Boolean;
 begin
   Result := False;
-  
+
   // Don't try too often
   if fTicker <= fLastTimeTrySetActionWalk + TRY_SET_ACTION_WALK_FREQ then
     Exit;
 
   UpdateLastTimeTrySetActionWalk;
 
-  newAction := TKMUnitActionWalkTo.Create(Self, aLocB, aActionType, aDistance, False, aTargetUnit, aTargetHouse,
-                                          tpNone, [], True, False, True);
+  // aSilent by default, as we consider fail could happen
+  var newAction := TKMUnitActionWalkTo.Create(Self, aLocB, aActionType, aDistance, False, aTargetUnit, aTargetHouse, tpNone, [], True, False, True);
 
   //Update action only if route was built, otherwise just keep using previous action
   if newAction.RouteBuilt then
   begin
     SetAction(newAction);
-    Exit(True);
+    Result := True;
   end
   else
-  begin
     newAction.Free;
-    Exit(False);
-  end;
 end;
 
 
