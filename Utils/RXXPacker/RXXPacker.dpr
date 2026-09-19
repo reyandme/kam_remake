2ca7f518f7db13cb7809875185fa5aaa1eeef014
kromster80 <kromster80@gmail.com>
2026-09-19 09:01:56 +0200

    Rearranged uses in DPR

--- Utils/RXXPacker/RXXPacker.dpr
+++ Utils/RXXPacker/RXXPacker.dpr
@@ -8,31 +8,6 @@ uses
   {$IFDEF MSWindows} Windows, {$ENDIF}
   {$IFDEF FPC} LResources, LCLIntf, {$ENDIF}
   
-  RXXPackerConsole in 'RXXPackerConsole.pas',
-  RXXPackerForm in 'RXXPackerForm.pas' {RXXForm1},
-  KM_RXXPacker in 'KM_RXXPacker.pas',
-  KM_RXXPackerManager in 'KM_RXXPackerManager.pas',
-
-  KM_IoPNG in '..\..\src\utils\io\KM_IoPNG.pas',
-  KM_Pics in '..\..\src\res\KM_Pics.pas',
-  KM_ResSprites in '..\..\src\res\KM_ResSprites.pas',
-  KM_ResSpritesEdit in '..\..\src\res\KM_ResSpritesEdit.pas',
-  KM_ResPalettes in '..\..\src\res\KM_ResPalettes.pas',
-  KM_ResTypes in '..\..\src\res\KM_ResTypes.pas',
-  KM_SoftShadows in '..\..\src\utils\algorithms\KM_SoftShadows.pas',
-  KM_Defaults in '..\..\src\common\KM_Defaults.pas',
-  KM_CommonTypes in '..\..\src\common\KM_CommonTypes.pas',
-  KM_CommonClasses in '..\..\src\common\KM_CommonClasses.pas',
-  KM_Points in '..\..\src\common\KM_Points.pas',
-  KM_CommonUtils in '..\..\src\utils\KM_CommonUtils.pas',
-  KromUtils in '..\..\src\utils\KromUtils.pas',
-  KM_FileIO in '..\..\src\utils\io\KM_FileIO.pas',
-  KM_Outline in '..\..\src\navmesh\KM_Outline.pas',
-  KM_PolySimplify in '..\..\src\navmesh\KM_PolySimplify.pas',
-  KM_Render in '..\..\src\render\KM_Render.pas',
-  KM_RenderControl in '..\..\src\render\KM_RenderControl.pas',
-  KM_BinPacking in '..\..\src\utils\algorithms\KM_BinPacking.pas',
-  PolyTriangulate in '..\..\src\ext\PolyTriangulate.pas',
   {$IFDEF FPC}
   BGRABitmap in '..\..\src\ext\BGRABitmap\BGRABitmap.pas',
   BGRAWinBitmap in '..\..\src\ext\BGRABitmap\BGRAWinBitmap.pas',
@@ -50,8 +25,30 @@ uses
   BGRAFilters in '..\..\src\ext\BGRABitmap\BGRAFilters.pas',
   BGRAText in '..\..\src\ext\BGRABitmap\BGRAText.pas',
   {$ENDIF}
-  dglOpenGL in '..\..\src\ext\dglOpenGL.pas',
-  KromOGLUtils in '..\..\src\utils\KromOGLUtils.pas';
+
+  KromUtils in '..\..\src\utils\KromUtils.pas',
+
+  KM_Defaults in '..\..\src\common\KM_Defaults.pas',
+  KM_CommonTypes in '..\..\src\common\KM_CommonTypes.pas',
+  KM_CommonClasses in '..\..\src\common\KM_CommonClasses.pas',
+  KM_CommonUtils in '..\..\src\utils\KM_CommonUtils.pas',
+  KM_FileIO in '..\..\src\utils\io\KM_FileIO.pas',
+  KM_IoPNG in '..\..\src\utils\io\KM_IoPNG.pas',
+  KM_Points in '..\..\src\common\KM_Points.pas',
+
+  RXXPackerConsole in 'RXXPackerConsole.pas',
+  RXXPackerForm in 'RXXPackerForm.pas' {RXXForm1},
+  KM_RXXPacker in 'KM_RXXPacker.pas',
+  KM_RXXPackerManager in 'KM_RXXPackerManager.pas',
+
+  KM_SoftShadows in '..\..\src\utils\algorithms\KM_SoftShadows.pas',
+  KM_BinPacking in '..\..\src\utils\algorithms\KM_BinPacking.pas',
+
+  KM_Pics in '..\..\src\res\KM_Pics.pas',
+  KM_ResSprites in '..\..\src\res\KM_ResSprites.pas',
+  KM_ResSpritesEdit in '..\..\src\res\KM_ResSpritesEdit.pas',
+  KM_ResPalettes in '..\..\src\res\KM_ResPalettes.pas',
+  KM_ResTypes in '..\..\src\res\KM_ResTypes.pas';
 
 
 {$IFDEF WDC}
